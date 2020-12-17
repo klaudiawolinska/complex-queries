7c1707eff150a2da541f77da926f09c9f48a51f9
Klaudia Szpyrka <klaudia.szpyrka@brainly.com>
2020-12-17 20:34:32 +0100

    triangle check script

---
+++ data/triangles.sql
@@ -0,0 +1,24 @@
+CREATE TABLE triangles
+(
+    A int,
+    B int,
+    C int
+)
+;
+
+INSERT INTO triangles
+VALUES (10, 20, 30)
+;
+
+INSERT INTO triangles
+VALUES (10, 10, 10)
+;
+
+INSERT INTO triangles
+VALUES (40, 20, 30)
+;
+
+INSERT INTO triangles
+VALUES (10, 20, 20)
+;
+
