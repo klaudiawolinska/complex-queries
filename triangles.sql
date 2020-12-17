5192afe986f03e4aa1f7c79548573d1a6510dd48
Klaudia Szpyrka <klaudia.szpyrka@brainly.com>
2020-12-17 20:27:17 +0100

    triangle check script

---
+++ triangles.sql
@@ -0,0 +1,10 @@
+--version: Oracle
+
+SELECT
+CASE
+  WHEN (A + B <= C) or (A + C <= B) or (B + C <= A) THEN 'Not A Triangle'
+  WHEN A = B and A = C and B = C THEN 'Equilateral'
+  WHEN (A = B and A != C) or (A = C and A != B) or (B = C and B != A) THEN 'Isosceles'
+  ELSE 'Scalene'
+END
+FROM triangles;
