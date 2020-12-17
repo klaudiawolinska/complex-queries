8585992f2438c3b3596f592cba849d34bb653d97
Klaudia Szpyrka <47977890+pumpkin27@users.noreply.github.com>
2020-12-17 20:35:37 +0100

    Update triangles.sql

--- data/triangles.sql
+++ data/triangles.sql
@@ -6,19 +6,7 @@ CREATE TABLE triangles
 )
 ;
 
-INSERT INTO triangles
-VALUES (10, 20, 30)
-;
-
-INSERT INTO triangles
-VALUES (10, 10, 10)
-;
-
-INSERT INTO triangles
-VALUES (40, 20, 30)
-;
-
-INSERT INTO triangles
-VALUES (10, 20, 20)
-;
-
+INSERT INTO triangles VALUES (10, 20, 30);
+INSERT INTO triangles VALUES (10, 10, 10);
+INSERT INTO triangles VALUES (40, 20, 30);
+INSERT INTO triangles VALUES (10, 20, 20);
