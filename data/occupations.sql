5b36b2a57b0e76824ebc9f6079cf3c4d6d95c0c8
Klaudia Szpyrka <klaudia.szpyrka@brainly.com>
2020-12-18 13:13:00 +0100

    transpose many to one

---
+++ data/occupations.sql
@@ -0,0 +1,17 @@
+CREATE TABLE occupations
+(
+    name       varchar(32),
+    occupation varchar(32)
+)
+;
+
+INSERT INTO occupations VALUES ('Frank', 'Doctor');
+INSERT INTO occupations VALUES ('Alicia', 'Doctor');
+INSERT INTO occupations VALUES ('Dan', 'Professor');
+INSERT INTO occupations VALUES ('Monica', 'Professor');
+INSERT INTO occupations VALUES ('Samantha', 'Professor');
+INSERT INTO occupations VALUES ('Joe', 'Singer');
+INSERT INTO occupations VALUES ('Gary', 'Actor');
+INSERT INTO occupations VALUES ('Lucy', 'Actor');
+INSERT INTO occupations VALUES ('Anna', 'Actor');
+INSERT INTO occupations VALUES ('Chuck', 'Actor');
---
+++ transpose-many-to-one.sql
@@ -0,0 +1,18 @@
+/*
+input data: data/occupations.sql
+helpful tutorial: http://www.sqlsnippets.com/en/topic-12190.html
+*/
+
+SELECT   doctor, 
+         professor, 
+         singer, 
+         actor
+FROM     ( 
+                  SELECT   occupation, 
+                           ROW_NUMBER() OVER(partition BY occupation ORDER BY name) AS row_num, 
+                           name 
+                  FROM     occupations) PIVOT (min(name) FOR occupation IN ('Doctor'   AS doctor,
+                                                                            'Professor' AS professor,
+                                                                            'Singer'    AS singer,
+                                                                            'Actor'     AS actor ) )
+ORDER BY row_num;
